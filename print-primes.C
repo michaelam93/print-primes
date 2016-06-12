515538bdad1732ac3c1bc68aab3376be763f3fbe
Michael[L] <michael.a.martinez93@gmail.com>
2016-06-12 15:40:46 -0500

    woops, first commit wasn't correct

--- print-primes.C
+++ print-primes.C
@@ -2,17 +2,25 @@
      Date:          May 21th, 2016
      Description:   A program to display all prime numbers between 1 - 1000   */
 #include <iostream>
+#include <cmath>
 using namespace std;
+
+bool isPrime(int a);
 
 int  main()
 {
-     for (int i = 2; i < 1000; i++){
-          for (int j = 2; j < i; j++){
-               if (!(i % j))
-                    break;
-               else if (i == ++j) 
-                    cout << i << " ";
-          }
-     }
-     cout << endl;
+  for (int i = 2; i < 1000; i++){
+    if (isPrime(i)){
+      cout << i << " ";
+    }
+  }
+  cout << endl;
+}
+
+bool isPrime(int a){
+  for (int i = 2; i <= sqrt(a); i++){
+    if ((a % i) == 0)
+      return false;
+  }
+  return true;
 }
